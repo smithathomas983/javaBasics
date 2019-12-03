e1c028a87cc20c630c3f110532e6855acdefe9d9
Smitha <smithathomas983@gmail.com>
2019-12-03 13:57:24 -0600

    conditional operator

---
+++ src/com/basicJava/ConditionalOperators.java
@@ -0,0 +1,18 @@
+package com.basicJava;
+
+public class ConditionalOperators {
+	//variable m=(expression)? value if True:value if False
+	// the conditional operator is also called ternary operator
+	public static void main(String[] args) {
+		String name="Smitha";
+		String gender;
+		gender=(name=="Smitha")?"Female":"Male";
+		System.out.println(gender);
+		
+		
+			
+		
+		
+	}
+
+}
